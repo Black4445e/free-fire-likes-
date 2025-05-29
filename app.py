a14d733552ec7be9c6ff6877a76cfa002abed949
Black4445e <brunomiguelsilvagomes19@gmail.com>
2025-05-29 19:24:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -236,4 +236,4 @@ def handle_requests():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=10000) 
